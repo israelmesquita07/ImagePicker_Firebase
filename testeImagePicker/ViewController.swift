f7777f7992e18f590bb34b6d8cc7d02e998e0a79
Rodrigo Longhi Guimarães <rguimaraes@alunos.utfpr.edu.br>
2017-10-20 13:35:56 -0200

    Initial Commit

---
+++ testeImagePicker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testeImagePicker
+//
+//  Created by Rodrigo Guimaraes on 2017-10-20.
+//  Copyright © 2017 RodrigoLG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
